365dd61d7058c77d53c86dad336a03c750c2f190
André Palma <andremarquespalma@gmail.com>
2021-04-12 16:25:45 +0100

    Clean Up Code

--- src/main/java/GameOfBeans.java
+++ src/main/java/GameOfBeans.java
@@ -2,33 +2,25 @@ import java.util.Arrays;
 
 public class GameOfBeans {
 
-    public static final int DEFAULT_DEPTH = 10;
-
     enum Player {JABA, PIETON}
 
     private final Player firstPlayer;
     private final int depth;
     private byte[] piles;
 
-    public GameOfBeans(Player firstPlayer, byte[] piles) {
-        this(firstPlayer, DEFAULT_DEPTH, piles);
-    }
-
     public GameOfBeans(Player firstPlayer, int depth, byte[] piles) {
         this.firstPlayer = firstPlayer;
         this.depth = depth;
         this.piles = piles;
     }
-    
+
     public int score() {
         switch (firstPlayer) {
             case JABA:
                 break;
             case PIETON:
-                short pietonWouldTake = pietonWouldTake(0, piles.length);
-                final int pietonPrefix = pietonWouldTake >>> Byte.SIZE;
-                final int pietonSuffix = (byte) pietonWouldTake;
-                piles = Arrays.copyOfRange(piles, pietonPrefix, piles.length - pietonSuffix);
+                byte pietonPlay = pietonPlay(0, piles.length);
+                piles = Arrays.copyOfRange(piles, toPrefix(pietonPlay), piles.length - toSuffix(pietonPlay));
                 break;
             default:
                 throw new IllegalStateException("No firstPlayer");
@@ -37,70 +29,75 @@ public class GameOfBeans {
         //there is a pattern whereby each row has -1 length than the prior
         int dpRows = Math.min(piles.length + 1, 2 * depth);
         short[][] dp = new short[dpRows][piles.length];
-        for (short[] scores : dp) {
-            //TODO mimimi first line overwritten below
-            Arrays.fill(scores, Short.MIN_VALUE);
-        }
         dp[0] = new short[piles.length + 1];
         for (int i = 0; i < piles.length; i++) {
             dp[1][i] = piles[i];
         }
+        for (int i = 2; i < dpRows; i++) {
+            Arrays.fill(dp[i], Short.MIN_VALUE);
+        }
 
-        //TODO consider changing exclusive to inclusive prefix and suffix for readability
         for (int length = 2; length <= piles.length; length++) {
             for (int prefixIndex = 0, suffixIndex = prefixIndex + length; suffixIndex <= piles.length; prefixIndex++, suffixIndex++) {
                 int maxPilesToRemove = Math.min(length, depth);
-                short prefixScore = Short.MIN_VALUE;
-                for (int removedPrefix = 1, totalScorePrefix = 0; removedPrefix <= maxPilesToRemove; removedPrefix++) {
-                    totalScorePrefix += piles[prefixIndex + (removedPrefix - 1)];
-                    short pietonWouldTake = pietonWouldTake(prefixIndex + removedPrefix, suffixIndex);
-                    final int pietonPrefix = pietonWouldTake >>> Byte.SIZE;
-                    final int pietonSuffix = (byte) pietonWouldTake;
-                    final int removedPiles = removedPrefix + pietonPrefix + pietonSuffix;
-                    prefixScore = (short) Math.max(prefixScore, dp[(length - removedPiles) % dpRows][prefixIndex + removedPrefix + pietonPrefix] + totalScorePrefix);
+                short score = Short.MIN_VALUE;
+                for (int removed = 1, prefixScore = 0, suffixScore = 0; removed <= maxPilesToRemove; removed++) {
+                    prefixScore += piles[prefixIndex + (removed - 1)];
+                    byte pietonPlay = pietonPlay(prefixIndex + removed, suffixIndex);
+                    byte pietonPrefix = toPrefix(pietonPlay);
+                    int removedPiles = removed + pietonPrefix + toSuffix(pietonPlay);
+                    score = (short) Math.max(score,
+                            dp[(length - removedPiles) % dpRows][prefixIndex + removed + pietonPrefix] + prefixScore);
+
+                    suffixScore += piles[suffixIndex - removed];
+                    pietonPlay = pietonPlay(prefixIndex, suffixIndex - removed);
+                    pietonPrefix = toPrefix(pietonPlay);
+                    removedPiles = removed + pietonPrefix + toSuffix(pietonPlay);
+                    score = (short) Math.max(score,
+                            dp[(length - removedPiles) % dpRows][prefixIndex + pietonPrefix] + suffixScore);
                 }
-                short suffixScore = Short.MIN_VALUE;
-                for (int removedSuffix = 1, totalScoreSuffix = 0; removedSuffix <= maxPilesToRemove; removedSuffix++) {
-                    totalScoreSuffix += piles[suffixIndex - removedSuffix];
-                    short pietonWouldTake = pietonWouldTake(prefixIndex, suffixIndex - removedSuffix);
-                    final int pietonPrefix = pietonWouldTake >>> Byte.SIZE;
-                    final int pietonSuffix = (byte) pietonWouldTake;
-                    final int removedPiles = removedSuffix + pietonPrefix + pietonSuffix;
-                    suffixScore = (short) Math.max(suffixScore, dp[(length - removedPiles) % dpRows][prefixIndex + pietonPrefix] + totalScoreSuffix);
-                }
-                dp[length % dpRows][prefixIndex] = (short) Math.max(prefixScore, suffixScore);
+                dp[length % dpRows][prefixIndex] = score;
             }
         }
         return dp[piles.length % dpRows][0];
     }
 
+    private byte toSuffix(byte pietonPlay) {
+        return pietonPlay > 0 ? pietonPlay : 0;
+    }
+
+    private byte toPrefix(byte pietonPlay) {
+        return pietonPlay < 0 ? (byte) -pietonPlay : 0;
+    }
+
     /**
      * TODO make me dp
+     *
      * @param ignoredPrefix ignore before ignoredPrefix (exclusive)
      * @param ignoredSuffix ignore after ignoredSuffix (exclusive)
-     * @return
+     * @return positive number if removed from suffix, negative otherwise, 0 if none is removed
      */
-    private short pietonWouldTake(int ignoredPrefix, int ignoredSuffix) {
+    private byte pietonPlay(int ignoredPrefix, int ignoredSuffix) {
         int pilesRemaining = ignoredSuffix - ignoredPrefix;
         int maxPilesToRemove = Math.min(pilesRemaining, depth);
-        int totalScorePrefix = Short.MIN_VALUE;
-        short maxPrefixRemoved = 0;
-        int totalScoreSuffix = Short.MIN_VALUE;
-        short maxSuffixRemoved = 0;
-        for (int removePrefix = 1, score = 0; removePrefix <= maxPilesToRemove; removePrefix++) {
+        short totalScorePrefix = Short.MIN_VALUE;
+        byte maxPrefixRemoved = 0;
+        short totalScoreSuffix = Short.MIN_VALUE;
+        byte maxSuffixRemoved = 0;
+        for (short removePrefix = 1, score = 0; removePrefix <= maxPilesToRemove; removePrefix++) {
             score += piles[ignoredPrefix + (removePrefix - 1)];
             if (score > totalScorePrefix) {
                 totalScorePrefix = score;
                 maxPrefixRemoved = (byte) removePrefix;
             }
         }
-        for (int removeSuffix = 1, score = 0; removeSuffix <= maxPilesToRemove; removeSuffix++) {
+        for (short removeSuffix = 1, score = 0; removeSuffix <= maxPilesToRemove; removeSuffix++) {
             score += piles[ignoredSuffix - removeSuffix];
             if (score > totalScoreSuffix) {
                 totalScoreSuffix = score;
                 maxSuffixRemoved = (byte) removeSuffix;
             }
         }
-        return totalScorePrefix >= totalScoreSuffix ? (short) (maxPrefixRemoved << Byte.SIZE) : maxSuffixRemoved;
+        return totalScorePrefix >= totalScoreSuffix ? (byte) -maxPrefixRemoved : maxSuffixRemoved;
     }
 }
